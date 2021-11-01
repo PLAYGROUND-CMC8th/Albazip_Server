2c0c7a8a14bfc2f94556aafcd9d4f5676282e3f7
Ju-Yeon Park <pjuyeon25@gmail.com>
2021-11-02 00:14:11 +0900

    make schedule utill

---
+++ src/module/scheduleUtil.js
@@ -0,0 +1,34 @@
+/*
+const { schedule, time, position, tasks } = require("../models");
+
+const duration = 90;
+const weekdays = ["월", "화", "수", "목", "금", "토", "일"];
+
+
+module.exports ={
+    makeASchedule : (positionId) => {
+        //90일 까지
+        //7일 반복하면서 time 보고 schedule 만들기
+
+    },
+    makeAHoliday : () => {
+        //90일 까지
+        //공휴일 정보 받아서 schedule 만들기
+
+    },
+    makeAllSchedule : () => {
+        //90일 뒤의 요일 확인하고
+        //공휴일 정보 확인하고 schedule 만들기
+
+
+    },
+    makeAllHoliday : () => {
+
+        //91일 꺼
+        //90일 뒤의 요일 확인하고
+        //해당 요일이 있는 time 보고 추가하기
+        //공휴일 확인하고
+        //schedule 만들기
+
+    }
+};*/
