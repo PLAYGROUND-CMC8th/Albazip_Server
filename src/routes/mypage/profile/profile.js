3512215829e6b5a73eb2dcebbc627d1c42ee5cb8
EC2 Default User <ec2-user@ip-172-31-32-167.us-east-2.compute.internal>
2021-11-06 10:52:21 +0000

    마이페이지 프로필 조회 오류 수정

--- src/routes/mypage/profile/profile.js
+++ src/routes/mypage/profile/profile.js
@@ -9,11 +9,11 @@ const { user, manager, worker, shop, position, time, board, task, schedule} = re
 router.get('/',userUtil.LoggedIn, async (req,res)=> {
 
     try {
-        let jobData, positionData;
+        let jobData, positionData, shopData;
         const userData = await user.findOne({where: {id: req.id}});
         if(req.job[0] == 'S'){
-            jobData = await manager.findOnd({where: {shop_id: req.job.substring(1)}});
-            const shopData = await shop.findOne({attributes: [ 'image_path' ], where: {id: req.job.substring(1)}});
+            jobData = await manager.findOne({where: {shop_id: req.job.substring(1)}});
+            shopData = await shop.findOne({attributes: [ 'image_path' ], where: {id: req.job.substring(1)}});
         } else {
             jobData = await worker.findOne({where: {position_id: req.job.substring(1)}});
             positionData = await position.findOne({attributes: [ 'image_path' ], where: {id: req.job.substring(1)}});
