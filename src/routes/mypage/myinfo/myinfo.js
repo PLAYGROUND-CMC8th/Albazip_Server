d4571b3597fa8fb1455d50691eff7d7f533e6927
Ju-Yeon Park <pjuyeon25@gmail.com>
2021-11-11 07:15:53 +0900

    마이페이지 근무자 내정보 퇴사요청하기

--- src/routes/mypage/myinfo/myinfo.js
+++ src/routes/mypage/myinfo/myinfo.js
@@ -7,9 +7,11 @@ var router = asyncify(express.Router());
 var userUtil = require('../../../module/userUtil');
 var positionUtil = require('../../../module/positionUtil');
 
+const { worker } = require('../../../models');
+
 
 // 마이페이지 하단 내정보
-router.get('/',userUtil.LoggedIn, async (req,res)=> {
+router.get('/', userUtil.LoggedIn, async (req,res)=> {
 
     const myinfoResult = await positionUtil.getWorkerInfo(req.job.substring(1));
     if (myinfoResult.code == "400"){
@@ -24,8 +26,44 @@ router.get('/',userUtil.LoggedIn, async (req,res)=> {
     });
     return;
 
+});
+
+// 마이페이지 하단 퇴사요청
+router.post('/resign/request', userUtil.LoggedIn, async (req,res)=> {
+
+    if(req.job[0] != 'P'){
+        res.json({
+            code: "202",
+            message: "퇴사요청은 근무자만 할 수 있습니다."
+        });
+        return;
+    }
+
+    worker.update({status: 2}, {where: {position_id: req.job.substring(1)}})
+        .then(updateWorker => {
+
+            console.log("success to update user resign request");
+            res.status(200).json({
+                code: "200",
+                message:"근무자 퇴사 요청을 신청했습니다."
+            });
+            return;
+        })
+        .catch((err) => {
+            console.log("update user resign request error", err );
+            res.json({
+                code: "400",
+                message: "근무자 퇴사 요청을 신청에 오류가 발생했습니다."
+            });
+            return;
+        });
 
 });
 
+// 마이페이지 하단 퇴사수락, 퇴사적용
+router.post('/resign/apply', userUtil.LoggedIn, async (req,res)=> {
+
+
+});
 
 module.exports = router;
