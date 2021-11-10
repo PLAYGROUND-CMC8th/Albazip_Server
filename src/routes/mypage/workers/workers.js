c79a65a6e41c9720ed8feba71dc4db4d2290e12b
Ju-Yeon Park <pjuyeon25@gmail.com>
2021-11-10 23:05:46 +0900

    마이페이지(관리자) 근무자 부재 시 근무자 선택 API 분리, 근무자 선택 API

--- src/routes/mypage/workers/workers.js
+++ src/routes/mypage/workers/workers.js
@@ -19,6 +19,90 @@ router.get('/',userUtil.LoggedIn, async (req,res)=> {
 // 마이페이지 > 하단 > 근무자 > 근무자 선택
 router.get('/:positionId',userUtil.LoggedIn, async (req,res)=> {
 
+    // 근무자 프로필
+    const positionProfiletResult = await positionUtil.getPositionProfile(req.params.positionId);
+    if( positionProfiletResult.code == "400" ){
+        res.json(positionProfiletResult);
+        return;
+    }
+
+    // 근무자 정보
+    const workerInfoResult = await positionUtil.getWorkerInfo(req.params.positionId);
+    if( workerInfoResult.code == "400" ){
+        res.json(workerInfoResult);
+        return;
+    }
+
+    // 근무자 포지션
+    const positionInfoResult = await positionUtil.getPositionInfo(req.params.positionId);
+    if( positionInfoResult.code == "400" ){
+        res.json(positionInfoResult);
+        return;
+    }
+
+    // 근무자 업무리스트
+    const positionTaskListResult = await taskUtil.getPositionTaskList(req.params.positionId);
+    if( positionTaskListResult.code == "400" ){
+        res.json(positionTaskListResult);
+        return;
+    }
+
+    res.json({
+        code: "200",
+        message: "근무자 조회에 성공했습니다. ",
+        data: {
+            positionProfile: positionProfiletResult.data,
+            workerInfo: workerInfoResult.data,
+            positionInfo: positionInfoResult.data,
+            positionTaskList: positionTaskListResult.data
+        }
+    });
+    return;
+
+});
+
+// 마이페이지 > 하단 > 근무자 > 근무자 선택
+router.get('/ne/:positionId',userUtil.LoggedIn, async (req,res)=> {
+
+    // 근무자 프로필
+    const positionProfiletResult = await positionUtil.getPositionProfile(req.params.positionId);
+    if( positionProfiletResult.code == "400" ){
+        res.json(positionProfiletResult);
+        return;
+    }
+
+    // 근무자 정보
+    const workerInfoResult = await positionUtil.getWorkerInfo(req.params.positionId);
+    if( workerInfoResult.code == "400" ){
+        res.json(workerInfoResult);
+        return;
+    }
+
+    // 근무자 포지션
+    const positionInfoResult = await positionUtil.getPositionInfo(req.params.positionId);
+    if( positionInfoResult.code == "400" ){
+        res.json(positionInfoResult);
+        return;
+    }
+
+    // 근무자 업무리스트
+    const positionTaskListResult = await taskUtil.getPositionTaskList(req.params.positionId);
+    if( positionTaskListResult.code == "400" ){
+        res.json(positionTaskListResult);
+        return;
+    }
+
+    res.json({
+        code: "200",
+        message: "근무자 조회에 성공했습니다. ",
+        data: {
+            positionProfile: positionProfiletResult.data,
+            workerInfo: workerInfoResult.data,
+            positionInfo: positionInfoResult.data,
+            positionTaskList: positionTaskListResult.data
+        }
+    });
+    return;
 
 });
 
@@ -33,6 +117,14 @@ router.get('/:positionId/profile',userUtil.LoggedIn, async (req,res)=> {
 
 // 마이페이지 > 하단 > 근무자 > 근무자 선택 > 하단 > 근무자 정보
 router.get('/:positionId/workerInfo',userUtil.LoggedIn, async (req,res)=> {
+
+    const workerInfoResult = await positionUtil.getWorkerInfo(req.params.positionId);
+    return res.json(workerInfoResult);
+
+});
+
+// 마이페이지 > 하단 > 근무자 > 근무자 선택 > 하단 > 근무자 정보
+router.get('/ne/:positionId/workerInfo',userUtil.LoggedIn, async (req,res)=> {
 
     const workerInfoResult = await positionUtil.getWorkerInfo(req.params.positionId);
     return res.json(workerInfoResult);
