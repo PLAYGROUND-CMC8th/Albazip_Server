40613f46136087d69e9ae02ac9b2ca3d083bdc10
Ju-Yeon Park <pjuyeon25@gmail.com>
2021-11-14 22:30:05 +0900

    푸시알림 테스트 API

--- src/module/pushAlarm.js
+++ src/module/pushAlarm.js
@@ -1,43 +1,100 @@
 const admin = require('firebase-admin')
-let serAccount = require('../config/albazip-5978d-firebase-adminsdk-dpw0u-4e17510d76.json')
+let serviceAccount = require('../config/albazip-cd574-firebase-adminsdk-bbzyx-ebe9929709.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
 
 module.exports = {
-    checkPhone : async (req, res, next) => {
-        let target_token = req.headers.registerToken;
-        let title = req.body.title;
-        let context = req.body.content;
+    testKite : async (req, res, next) => {
 
-        if (!target_token) {
+        let target_token_array = ["evJXnjz_R9qmrlzWK6ZG-g:APA91bE43OlFENm9BQJ3wISUYEHWUUZmSMjmUcP6H5dsJDYwVVBE6zGh2r6vZrPF2zHmYbsKBHtr-AvDUZuaMHMwzocvSQAUnSsflZTeNe485XsjKm5VLVfSOjqwzdmBg32aknkt3p8E"];
+
+        if (!target_token_array) {
             console.log("no target token: ", target_token);
             res.json({
                 code: "202",
                 message: "등록된 기기의 토큰이 유효하지 않습니다. "
             })
         } else {
+
             let message = {
-                data: {
-                    title: title,
-                    body: context
+                notification: {
+                    title: "카이트 푸시알림 테스트",
+                    body: "카이트 푸시알림 테스트 중입니다 ~"
                 },
-                token: target_token
+                data: {
+                    flag: "0",
+                    status: "2",
+                    info: {
+                        name: "카이트",
+                        job: "Android Developer"
+                    }
+                }
             };
 
-            admin.initializeApp({
-                credential: admin.credential.cert(serAccount),
-            });
-
-            admin
-                .messaging()
-                .send(message)
-                .then(function (response) {
+            admin.messaging().sendToDevice(target_token_array, message)
+                .then((response) => {
                     console.log("push alarm success: ", response)
+                    return res.json({
+                        code: "200",
+                        message:"푸시알림 발송을 성공했습니다."
+                    });
                 })
-                .catch(function (err) {
-                    console.log("error push alarm server: ", err)
-                    return response.status(400).json({
+                .catch((error) => {
+                    console.log("error push alarm server: ", error)
+                    return res.json({
+                        code: "400",
                         message:"푸시알림 발송에 오류가 발생했습니다."
                     });
-                });
+                })
+            next();
+        }
+    },
+
+    testChobi : async (req, res, next) => {
+
+        let target_token_array = ["dRxMLg1G2U0tijL38ZnZ2h:APA91bHB3t6hT2207-rf1RkerUOFArdJgvCKJN0pltLI-j1XMHCPpW2idr6KfLseipd8tli-BUAwrqPNuG8i43x4Ct06L_g45hwWde6aMqBpmB5P2Cs6hLd5yOTRI5CyfjgaqNSwIak5"];
+
+        if (!target_token_array) {
+            console.log("no target token: ", target_token);
+            res.json({
+                code: "202",
+                message: "등록된 기기의 토큰이 유효하지 않습니다. "
+            })
+        } else {
+
+            let message = {
+                notification: {
+                    title: "초비 푸시알림 테스트",
+                    body: "초비 푸시알림 테스트 중입니다 ~"
+                },
+                data: {
+                    flag: "0",
+                    status: "2",
+                    info: {
+                        name: "초비",
+                        job: "iOS Developer"
+                    }
+                }
+            };
+
+
+            admin.messaging().sendToDevice(target_token_array, message)
+                .then((response) => {
+                    console.log("push alarm success: ", response)
+                    return res.json({
+                        code: "200",
+                        message:"푸시알림 발송을 성공했습니다."
+                    });
+                })
+                .catch((error) => {
+                    console.log("error push alarm server: ", error)
+                    return res.json({
+                        code: "400",
+                        message:"푸시알림 발송에 오류가 발생했습니다."
+                    });
+                })
             next();
         }
     }
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,24 @@ router.use('/shop', require('./shop'));
 router.use('/position', require('./position'));
 router.use('/schedule', require('./schedule'));
 router.use('/mypage', require('./mypage'));
+router.use('/home', require('./home'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// test pushAlarm
+var pushAlarm = require('../module/pushAlarm');
+
+router.get('/kite', pushAlarm.testKite, async (req,res)=>{
+  return;
+});
+
+router.get('/chobi', pushAlarm.testChobi, async (req,res)=>{
+  return;
+});
+
 
 module.exports = router;
