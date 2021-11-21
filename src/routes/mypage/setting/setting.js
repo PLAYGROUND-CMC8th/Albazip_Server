9b5e1665ad680d078abbf18496d3dffa2b69d3e2
Ju-Yeon Park <pjuyeon25@gmail.com>
2021-11-21 14:18:52 +0900

    마이페이지 설정 내정보 수정시 관리자, 근무자 관계에도 업데이트 처리

--- src/routes/mypage/setting/setting.js
+++ src/routes/mypage/setting/setting.js
@@ -5,7 +5,8 @@ var voca = require('voca');
 
 var userUtil = require('../../../module/userUtil');
 
-const { user } = require('../../../models');
+const { user, manager, worker } = require('../../../models');
+
 
 // 마이페이지 > 설정 > 나의정보 조회
 router.get('/myinfo',userUtil.LoggedIn, async (req,res)=> {
@@ -38,9 +39,9 @@ router.get('/myinfo',userUtil.LoggedIn, async (req,res)=> {
 // 마이페이지 > 설정 > 나의정보 > 나의정보 업데이트
 router.post('/myinfo',userUtil.LoggedIn, async (req,res)=> {
 
-    const { firstName, lastName, birthyear, gender } = req.body;
+    const {firstName, lastName, birthyear, gender} = req.body;
 
-    if(!firstName || !lastName || !birthyear || !gender){
+    if (!firstName || !lastName || !birthyear || !gender) {
         console.log("not enough parameter");
         res.json({
             code: "202",
@@ -49,29 +50,58 @@ router.post('/myinfo',userUtil.LoggedIn, async (req,res)=> {
         return;
     }
 
-    user.update({
-        first_name: firstName,
-        last_name: lastName,
-        birthyear: birthyear,
-        gender: gender
-    }, {where: {id: req.id}})
-        .then(updateUser => {
-
-            console.log("success to update setting user myinfo");
-            res.status(200).json({
-                code: "200",
-                message:"사용자 내정보 변경을 성공했습니다."
-            });
-            return;
-        })
-        .catch((err) => {
-            console.log("update setting user myinfo error", err );
-            res.json({
-                code: "400",
-                message: "사용자 내정보 변경에 오류가 발생했습니다."
-            });
-            return;
+    try {
+        await user.update({
+            first_name: firstName, last_name: lastName,
+            birthyear: birthyear, gender: gender
+        }, {where: {id: req.id}});
+        console.log("success to update user info");
+    }
+    catch(err) {
+        console.log("update user info error", err );
+        res.json({
+            code: "400",
+            message: "사용자 유저정보 변경에 오류가 발생했습니다."
         });
+        return;
+    }
+
+    try {
+        await manager.update({
+            user_first_name: firstName,
+            user_last_name: lastName
+        }, {where: {user_id: req.id}});
+        console.log("success to update user manager info");
+    }
+    catch(err) {
+        console.log("update user manager info error", err );
+        res.json({
+            code: "400",
+            message: "사용자의 관리자정보 변경에 오류가 발생했습니다."
+        });
+        return;
+    }
+
+    try {
+        await worker.update({
+            user_first_name: firstName
+        }, {where: {user_id: req.id}});
+        console.log("success to update user worker info");
+    }
+    catch(err) {
+        console.log("update user worker info error", err );
+        res.json({
+            code: "400",
+            message: "사용자의 근무자정보 변경에 오류가 발생했습니다."
+        });
+        return;
+    }
+
+    console.log("success to update setting myinfo");
+    res.json({
+       code: "200",
+       message: "사용자의 근무자 내정보 변경을 성공했습니다."
+    });
 
 });
 
