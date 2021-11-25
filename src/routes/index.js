73da3b3e188dff42b94eda854d73d496b360ca11
Ju-Yeon Park <pjuyeon25@gmail.com>
2021-11-25 23:57:03 +0900

    첫 진입시 토큰 유효성 확인 API

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,6 +15,45 @@ router.get('/', function(req, res, next) {
 });
 
 
+// 첫 집입시 토큰 유효성 확인
+var userUtil = require('../module/userUtil');
+var { manager, worker } = require('../models');
+
+router.get('/token', userUtil.LoggedIn, async (req,res)=>{
+
+  try {
+    let jobCount;
+    if (req.job[0] == "M")
+      jobCount = await manager.count({where: {id: req.job.substring(1)}});
+    else if (req.job[0] == "W")
+      jobCount = await worker.count({where: {id: req.job.substring(1)}});
+
+    if (jobCount > 0) {
+      console.log("valid token");
+      return res.json({
+        code: "200",
+        message: "유효한 토큰입니다.",
+        status: 1
+      });
+    } else {
+      console.log("invalid token");
+      return res.json({
+        code: "202",
+        message: "유효하지 않은 토큰입니다.",
+        status: 0
+      });
+    }
+  }
+  catch(err) {
+    console.log("token validation error", err);
+    return res.json({
+      code: "400",
+      message: "토큰 유효성 확인에 오류가 발생했습니다."
+    });
+  }
+
+});
+
 // test pushAlarm
 var pushAlarm = require('../module/pushAlarm');
 
