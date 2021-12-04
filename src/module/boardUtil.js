f9adafb190248a6b60fd3a31806893b92fec29af
Ju-Yeon Park <pjuyeon25@gmail.com>
2021-12-04 16:47:12 +0900

    마이페이지 게시글, 소통창 게시글 분리

--- src/module/boardUtil.js
+++ src/module/boardUtil.js
@@ -1,11 +1,138 @@
-const { user, position, board, comment, worker, manager } = require('../models');
+const { user, position, board, board_image, comment, worker, manager } = require('../models');
 
 const pagesize = 20;
 
 module.exports = {
+    // 소통창 > 게시글
+    getPost: async  (reqJob, reqPage) => {
+
+        try {
+            const offset = 0 + (reqPage - 1) * pagesize
+            console.log("request post page", reqPage);
+
+            let shopId;
+            if (reqJob[0] == "M") {
+                let managerData = await manager.findOne({attributes: ['shop_id'], where: {id: reqJob.substring(1)}});
+                shopId = managerData.shop_id
+            } else if (reqJob[0] == "W") {
+                let workerData = await worker.findOne({attributes: ['position_id'], where: {id: reqJob.substring(1)}});
+                let positionData = await position.findOne({
+                    attributes: ['shop_id'],
+                    where: {id: workerData.position_id}
+                });
+                shopId = positionData.shop_id;
+            }
+
+            let postData;
+            try {
+                postData = await board.findAll({
+                    offset: offset,
+                    limit: pagesize,
+                    attributes: ['id', 'title', 'content', 'writer_job', 'register_date'],
+                    where: {status: 1, shop_id: shopId},
+                    order: [['register_date', 'DESC']]
+                });
+                console.log("success to get recent post");
+            } catch (err) {
+                console.log("get recent post error", err);
+                return result = {
+                    code: "400",
+                    message: "게시글 조회에 오류가 발생했습니다.",
+                };
+            }
+
+            let postInfo = [];
+            if (postData) {
+                for (const pdata of postData) {
+
+                    // 작성자
+                    let writerJob, writerName, writerImage;
+                    if (pdata.writer_job[0] == 'M') {
+                        let managerData = await manager.findOne({where: {id: pdata.writer_job.substring(1)}});
+                        writerJob = "사장님";
+                        writerName = managerData.user_last_name + managerData.user_first_name;
+                        writerImage = managerData.image_path;
+
+                    } else {
+                        try {
+                            let workerData = await worker.findOne({where: {id: pdata.writer_job.substring(1)}});
+                            writerJob = workerData.position_title;
+                            writerName = workerData.user_first_name;
+                            writerImage = workerData.image_path;
+
+                        } catch (err) {
+                            writerJob = null;
+                            writerName = null;
+                            writerImage = null;
+                        }
+                    }
+                    console.log("success to get writer data");
+
+                    // 게시글 이미지
+                    let image;
+                    try {
+                        let boardImageData = await board_image.findOne({where: {board_id: pdata.id}});
+                        image = boardImageData.image_paht;
+                    } catch (err) {
+                        image = null;
+                    }
+
+                    // 24시간 이내
+                    let in24Hour = 0;
+                    const nowTime = new Date();
+                    const writeTime = new Date(pdata.register_date);
+                    const passTime = parseInt(nowTime.getTime() - writeTime.getTime());
+                    const passHour = Math.round(passTime / (1000 * 60 * 60));
+                    if (passHour < 24) in24Hour = 1;
+
+                    // 댓글수
+                    /* let commentCount;
+                   try {
+                       let count = await comment.count({where: {status: [1, 2], board_id: pdata.id}});
+                       commentCount = count;
+                       console.log("success to get comment count data");
+
+                   } catch (err) {
+                       commentCount = 0;
+                       console.log("get comment count data error", err);
+                   }*/
+
+                    let data = {
+                        id: pdata.id,
+                        writerJob: writerJob,
+                        writerName: writerName,
+                        writerImage: writerImage,
+                        title: pdata.title,
+                        content: pdata.content,
+                        image: image,
+                        //commentCount: commentCount,
+                        writeIn24Hour: in24Hour,
+                        writeInHour: passHour,
+                        writeDate: pdata.register_date
+                    }
+                    postInfo.push(data);
+                }
+            }
+            console.log("success to get post data");
+            return {
+                code: "200",
+                message: "게시글 조회에 성공했습니다.",
+                page: reqPage,
+                data: postInfo
+            };
+        }
+        catch (err) {
+            console.log("get post data error", err);
+            return {
+                code: "400",
+                message: "게시글 조회에 오류가 발생했습니다."
+            };
+        }
+
+    },
 
     // 마이페이지 > 하단 > 작성글 > 게시글
-    getPost: async (reqJob, reqPage) => {
+    getMyPost: async (reqJob, reqPage) => {
 
         try {
             const offset = 0 + (reqPage - 1) * pagesize
@@ -23,23 +150,13 @@ module.exports = {
 
             let postData;
             try {
-                if(reqJob) {
-                    postData = await board.findAll({
-                        offset: offset,
-                        limit: pagesize,
-                        attributes: ['id', 'title', 'content', 'register_date'],
-                        where: {writer_job: reqJob, status: 1, shop_id: shopId},
-                        order: [['register_date', 'DESC']]
-                    });
-                } else {
-                    postData = await board.findAll({
-                        offset: offset,
-                        limit: pagesize,
-                        attributes: ['id', 'title', 'content', 'writer_job', 'register_date'],
-                        where: {status: 1, shop_id: shopId},
-                        order: [['register_date', 'DESC']]
-                    });
-                }
+                postData = await board.findAll({
+                    offset: offset,
+                    limit: pagesize,
+                    attributes: ['id', 'title', 'content', 'register_date'],
+                    where: {writer_job: reqJob, status: 1, shop_id: shopId},
+                    order: [['register_date', 'DESC']]
+                });
                 console.log("success to get recent post");
 
             } catch (err) {
@@ -51,32 +168,24 @@ module.exports = {
             }
 
             let writerJob, writerName;
-            if(reqJob) {
-                if (reqJob[0] == 'M') {
-                    let managerData = await manager.findOne({where: {id: reqJob.substring(1)}});
-                    writerJob = "사장님";
-                    writerName = managerData.user_last_name + managerData.user_first_name;
+            if (reqJob[0] == 'M') {
+                let managerData = await manager.findOne({where: {id: reqJob.substring(1)}});
+                writerJob = "사장님";
+                writerName = managerData.user_last_name + managerData.user_first_name;
 
-                } else {
-                    try {
-                        let workerData = await worker.findOne({where: {id: reqJob.substring(1)}});
-                        writerJob = workerData.position_title;
-                        writerName = workerData.user_first_name;
+            } else if (reqJob[0] == 'W') {
+                let workerData = await worker.findOne({where: {id: reqJob.substring(1)}});
+                writerJob = workerData.position_title;
+                writerName = workerData.user_first_name;
 
-                    } catch (err) {
-                        writerJob = null;
-                        writerName = null;
-                    }
-                }
-                console.log("success to get writer data");
             }
-
+            console.log("success to get writer data");
 
             let postInfo = [];
             if (postData) {
                 for (const pdata of postData) {
 
-                    let commentCount;
+                   /* let commentCount;
                     try {
                         let count = await comment.count({where: {status: [1, 2], board_id: pdata.id}});
                         commentCount = count;
@@ -85,26 +194,7 @@ module.exports = {
                     } catch (err) {
                         commentCount = 0;
                         console.log("get comment count data error", err);
-                    }
-
-                    if(!reqJob){
-                        if (pdata.writer_job[0] == 'M') {
-                            let managerData = await manager.findOne({where: {id: reqJob.substring(1)}});
-                            writerJob = "사장님";
-                            writerName = managerData.user_last_name + managerData.user_first_name;
-
-                        } else {
-                            try {
-                                let workerData = await worker.findOne({where: {id: reqJob.substring(1)}});
-                                writerJob = workerData.position_title;
-                                writerName = workerData.user_first_name;
-
-                            } catch (err) {
-                                writerJob = null;
-                                writerName = null;
-                            }
-                        }
-                    }
+                    }*/
 
                     let data = {
                         id: pdata.id,
@@ -112,7 +202,7 @@ module.exports = {
                         writerName: writerName,
                         title: pdata.title,
                         content: pdata.content,
-                        commentCount: commentCount,
+                        //commentCount: commentCount,
                         registerDate: pdata.register_date
                     }
                     postInfo.push(data);
@@ -137,7 +227,7 @@ module.exports = {
 
     },
 
-    // 홈 > 하단 > 소통창
+
     // 소통창 > 공지사항
     getNotice: async (reqJob, reqPage, confirm) => {
 
@@ -236,6 +326,7 @@ module.exports = {
         }
     },
 
+    //소통창 > 검색 > 공지사항
     searchNotice: async (reqJob, reqPage, searchWord) => {
 
         try {
@@ -295,5 +386,7 @@ module.exports = {
                 page: reqPage
             };
         }
-    }
+    },
+
+    //소통창 > 검색 > 게시글
 }
